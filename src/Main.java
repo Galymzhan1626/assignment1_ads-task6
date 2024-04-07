bbedae892550c60e9f1833e9889a8ba7505fae1f
Galymzhan1626 <154108707+Galymzhan1626@users.noreply.github.com>
2024-04-08 01:53:59 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * This method calculates a value in a sequence based on the inputs 'n' and 'm'.
+     * It uses recursion to multiply 'n' by the result of the sequence with 'm-1'.
+     * The base cases handle when 'n' is 1 or 'm' is 0, returning 1, and when 'n' is 0, returning 0.
+     *
+     * @param n The base integer of the sequence.
+     * @param m The exponent representing the depth of the sequence calculation.
+     * @return The calculated value of the sequence for 'n' and 'm'.
+     */
     public static int sequence(int n, int m){
         if(n==1 || m==0){
             return 1;
